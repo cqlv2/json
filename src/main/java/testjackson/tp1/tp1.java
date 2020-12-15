2f50425e62771fa611cca6f8867d817aa795db5e
sylvain amianto <samianto@gmail.com>
2020-12-15 14:50:58 +0100

    affichage des datas

--- src/main/java/testjackson/tp1/tp1.java
+++ src/main/java/testjackson/tp1/tp1.java
@@ -13,24 +13,28 @@ public class tp1 {
 
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 
-		
 		ObjectMapper mapper = new ObjectMapper();
 		CollectionType collType = mapper.getTypeFactory().constructCollectionType(List.class, Entreprise.class);
-		List<Entreprise> listeEntreprise = mapper.readValue(new File("C:/Users/cql-v/eclipse-workspace/testjackson/src/main/resources/json1.json"), collType);
-		
-		
-		
-		
+		List<Entreprise> listeEntreprise = mapper.readValue(
+				new File("C:/Users/cql-v/eclipse-workspace/testjackson/src/main/resources/json1.json"), collType);
+
+		// affichage
 		for (Entreprise entreprise : listeEntreprise) {
-			System.out.println(entreprise.getNom());
-			System.out.println(entreprise.getChiffreAffaire().get(0).getChiffre());
-			
+			System.out.println("entreprise : " + entreprise.getNom());
+			for (Fondateur f : entreprise.getFondateur()) {
+				System.out.println("fondateur nom : " + f.getNom());
+				System.out.println("fondateur date de naissance : " + f.getDateNaissance());
+				System.out.println("fondateur lieu de naissance : " + f.getLieuNaissance());
+			}
+			System.out.println("siege social site: " + entreprise.getSiegeSocial().getSite());
+			System.out.println("siege social ville: " + entreprise.getSiegeSocial().getVille());
+			System.out.println("siege social etat: " + entreprise.getSiegeSocial().getEtat());
+			System.out.println("siege social pays: " + entreprise.getSiegeSocial().getPays());
+			for (ChiffreAffaire ca : entreprise.getChiffreAffaire()) {
+				System.out.println(ca.getAnnee() + " : " + ca.getChiffre() + ca.getUnite());
+			}
 		}
-		
-		
+
 	}
 
-
-	
-	
 }
